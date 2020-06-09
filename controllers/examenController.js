82ec926d4f08ff7710c89fdf28570df22bfb2503
DouglasWZ <51092517+DouglasWZ@users.noreply.github.com>
2020-06-09 15:52:39 -0600

    declaracion de variables y calculos para realizar las operaciones requeridas

--- controllers/examenController.js
+++ controllers/examenController.js
@@ -1,3 +1,23 @@
 exports.home = (req,res, next) => { 
     res.render("crear_examen");
 };
+
+// Variables para realizar los calculos
+exports.nuevoExamen = (req, res, next) => {
+    const { prestamo } = req.body;
+    const { años } = req.body;
+    const { interes } = req.body;
+    const { amortizacion } = req.body;
+    const { cuota } = req.body;
+    const { capital } = req.body;
+
+    if(nullprestamo && nullaños && nullinteres){
+        Console.log("Debe llenar todas las casilla");
+    } else {
+        res.send('Calculo realizado correctamente.');
+    }
+// Realizar los Calculos necesarios
+    capital = cuota - interes;
+    cuota = (capital * interes)/1-(1+0.06)^-4;
+}
+
